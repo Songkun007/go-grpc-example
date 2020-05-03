8c966cd5884207dca6e66420acbacaf9eb94ad87
kunsong <1548604122@qq.com>
2020-05-03 15:56:52 +0800

    grpc-demo

---
+++ client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pd "github.com/Songkun007/go-grpc-example/proto"
+	"google.golang.org/grpc"
+)
+
+const PORT = "9001"
+
+func main() {
+	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("grpc.Dial err: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewSearchServiceClient(conn)
+	resp, err := client.Search(context.Background(), &pb.SearchRequest{
+		Request: "gRPC",
+	})
+	if err != nil {
+		log.Fatalf("client.Search err: %v", err)
+	}
+
+	log.Printf("resp: %s", resp.GetResponse())
+}
