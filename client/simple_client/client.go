82f98888ddd5425e61424b6a021d92e6b49ddcc5
kunsong <1548604122@qq.com>
2020-05-05 19:43:36 +0800

    支持 TLS 证书认证

--- client/simple_client/client.go
+++ client/simple_client/client.go
@@ -6,12 +6,21 @@ import (
 
 	pb "github.com/Songkun007/go-grpc-example/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 const PORT = "9001"
 
 func main() {
-	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	// 支持 TLS 证书认证
+	// credentials.NewServerTLSFromFile(certFile, serverNameOverride string)：
+	// 根据客户端输入的证书文件和密钥构造 TLS 凭证。serverNameOverride 为服务名称
+	c, err := credentials.NewClientTLSFromFile("../../conf/server.pem", "go-grpc-example")
+	if err != nil {
+		log.Fatalf("credentials.NewClientTLSFromFile err: %v", err)
+	}
+
+	conn, err := grpc.Dial(":"+PORT, grpc.WithTransportCredentials(c))
 	if err != nil {
 		log.Fatalf("grpc.Dial err: %v", err)
 	}
--- server/simple_server/server.go
+++ server/simple_server/server.go
@@ -7,6 +7,7 @@ import (
 
 	pb "github.com/Songkun007/go-grpc-example/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type SearchService struct{}
@@ -18,7 +19,13 @@ func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.Se
 const PORT = "9001"
 
 func main() {
-	server := grpc.NewServer()
+	// 支持 TLS 证书认证
+	// credentials.NewServerTLSFromFile：根据服务端输入的证书文件和密钥构造 TLS 凭证
+	c, err := credentials.NewServerTLSFromFile("../../conf/server.pem", "../../conf/server.key")
+	if err != nil {
+		log.Fatalf("credentials.NewServerTLSFromFile err: %v", err)
+	}
+	server := grpc.NewServer(grpc.Creds(c))
 	// 将 SearchService（其包含需要被调用的服务端接口）注册到 gRPC Server 的内部注册中心。
 	// 这样可以在接受到请求时，通过内部的服务发现，发现该服务端接口并转接进行逻辑处理
 	pb.RegisterSearchServiceServer(server, &SearchService{})
